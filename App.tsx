6ee381b195f1686981139e40692cff79d9e46e35
Jackson dos Santos <jackson1santos@proton.me>
2025-03-25 16:35:58 -0300

    ✨ feat: implementa componentes estilizados

--- App.tsx
+++ App.tsx
@@ -2,9 +2,5 @@ import { Groups } from '@screens/Groups'
 import { View } from 'react-native'
 
 export default function App() {
-  return (
-    <View style={{ marginTop: 95 }}>
-      <Groups />
-    </View>
-  )
+  return <Groups />
 }
