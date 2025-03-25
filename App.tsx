e7ac65b59f15f23133c7924938f752b24349f841
Jackson dos Santos <jackson1santos@proton.me>
2025-03-25 16:19:22 -0300

    ✨ feat:  implementa styled-components

--- App.tsx
+++ App.tsx
@@ -1,5 +1,10 @@
 import { Groups } from '@screens/Groups'
+import { View } from 'react-native'
 
 export default function App() {
-  return <Groups />
+  return (
+    <View style={{ marginTop: 95 }}>
+      <Groups />
+    </View>
+  )
 }
