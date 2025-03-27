f4264b1caf7fe1ea7bdd3ff529cb49ca38b33a89
Jackson dos Santos <jackson1santos@proton.me>
2025-03-27 01:19:45 -0300

    ✨ feat: implementa biblioteca de icones e botao de voltar

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,19 @@
-import { Container, Logo } from './styles'
+import { BackButton, BackIcon, Container, Logo } from './styles'
 import logoImg from '@assets/logo.png'
+type Props = {
+  showBackButton?: boolean
+}
 
-export function Header() {
+export function Header({ showBackButton = false }: Props) {
   return (
     <>
       <Container>
+        {showBackButton && (
+          <BackButton>
+            <BackIcon />
+          </BackButton>
+        )}
+
         <Logo source={logoImg} />
       </Container>
     </>
