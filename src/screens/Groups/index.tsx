7ca37a860c53e76eeb0539e8f6cde465d4626497
Jackson dos Santos <jackson1santos@proton.me>
2025-03-28 23:28:03 -0300

    ✨ feat(single): Lista grupos

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -2,14 +2,29 @@ import { GroupCard } from '@components/GroupCard'
 import { Container } from './styles'
 import { Header } from '@components/Header'
 import { Highlight } from '@components/Highlight'
+import { useState } from 'react'
+import { FlatList, Text } from 'react-native'
 
 export function Groups() {
+  const [groups, setGroups] = useState([
+    'Liga da Justiça',
+    'Vingadores',
+    'Defensores',
+  ])
+
   return (
     <Container>
       <Header showBackButton />
       <Highlight title="Turmas" subtitle="jogue com a sua turma" />
-
-      <GroupCard title="Galera do Ignite" />
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => item}
+        showsVerticalScrollIndicator={false}
+        ListEmptyComponent={() => (
+          <Text>Adicione participantes à sua lista de presença.</Text>
+        )}
+        renderItem={({ item }) => <GroupCard title={item} />}
+      />
     </Container>
   )
 }
