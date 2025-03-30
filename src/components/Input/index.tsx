685866b3a362b47598ca0dae461b85ab34829838
Jackson dos Santos <jackson1santos@proton.me>
2025-03-30 01:42:38 -0300

    ✨ feat(multi): cria componente Input

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,6 @@
+import type { TextInputProps } from 'react-native'
+import { Container } from './styles'
+
+export function Input({ ...rest }: TextInputProps) {
+  return <Container {...rest} />
+}
--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -2,6 +2,7 @@ import { Header } from '@components/Header'
 import { Container, Content, Icon } from './styles'
 import { Highlight } from '@components/Highlight'
 import { Button } from '@components/Button'
+import { Input } from '@components/Input'
 
 export function NewGroup() {
   return (
@@ -14,7 +15,9 @@ export function NewGroup() {
           subtitle="crie a turma para adicionar as pessoas"
         />
 
-        <Button title="Criar" />
+        <Input />
+
+        <Button title="Criar" style={{ marginTop: 20 }} />
       </Content>
     </Container>
   )
