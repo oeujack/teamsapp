d2497e7e58632b06a7a738ac095edf1d94e54791
Jackson dos Santos <jackson1santos@proton.me>
2025-04-01 10:43:54 -0300

    ✨ feat(multi): armazenando novos grupos

--- babel.config.js
+++ babel.config.js
@@ -11,6 +11,7 @@ module.exports = function (api) {
             '@assets': './src/assets',
             '@components': './src/components',
             '@screens': './src/screens',
+            '@storage': './src/storage',
           }
         }
       ]
